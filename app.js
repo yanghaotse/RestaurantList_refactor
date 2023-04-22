9b6215b8377dfaf45cb423aa0a6710937caf9af7
yang haotse <ha91252@gmail.com>
2023-04-22 22:23:57 +0800

    refactor: sort route

--- app.js
+++ app.js
@@ -18,47 +18,6 @@ app.use(express.static('public')) //使用靜態檔案
 app.use(methodOverride('_method'))
 app.use(routes)
 
-app.get("/sort", (req, res) => {
-  const sort = req.query.sort
-  switch (sort) {
-    case "A->Z":
-      return Restaurant.find()
-        .lean()
-        .sort({name: 'asc'}) //sort()排序，asc正序/desc反序
-        .then((restaurants) => res.render("index", { restaurants, sort }))
-        .catch(error => console.log(error))
-    case "Z->A":
-      Restaurant.find()
-        .lean()
-        .sort({name: 'desc'}) //sort()排序，asc正序/desc反序
-        .then((restaurants) => res.render("index", { restaurants, sort }))
-        .catch(error => console.log(error))
-      break
-    case "類別":
-      Restaurant.find()
-        .lean()
-        .sort({category: 'asc'}) //sort()排序，asc正序/desc反序
-        .then((restaurants) => res.render("index", { restaurants, sort }))
-        .catch(error => console.log(error))
-      break
-     case "地區":
-      Restaurant.find()
-        .lean()
-        .sort({location: 'asc'}) //sort()排序，asc正序/desc反序
-        .then((restaurants) => res.render("index", { restaurants, sort }))
-        .catch(error => console.log(error))
-      break 
-  }
-})
-
 app.listen( port, () => {
   console.log(`Express is running on http://localhost:${port}`)
 })
-// /?sort-select=asc
-// router.get('/',(req, res) => {
-//   Restaurant.find()
-//     .lean()
-//     .sort({name: 'asc'}) //sort()排序，asc正序/desc反序
-//     .then((restaurants) => res.render("index", { restaurants }))
-//     .catch(error => console.log(error))
-// })
--- routes/models/home.js
+++ routes/models/home.js
@@ -10,11 +10,39 @@ router.get('/',(req, res) => {
     .catch(error => console.log(error))
 })
 
-// router.get("/sort/?sort-select=asc", (req, res) => {
-//   console.log('params:',req.params)
-//   console.log('query:',req.query)
+router.get("/sort", (req, res) => {
+  const sort = req.query.sort
+  switch (sort) {
+    case "A->Z":
+      return Restaurant.find()
+        .lean()
+        .sort({name: 'asc'}) //sort()排序，asc正序/desc反序
+        .then((restaurants) => res.render("index", { restaurants, sort }))
+        .catch(error => console.log(error))
+    case "Z->A":
+      Restaurant.find()
+        .lean()
+        .sort({name: 'desc'}) //sort()排序，asc正序/desc反序
+        .then((restaurants) => res.render("index", { restaurants, sort }))
+        .catch(error => console.log(error))
+      break
+    case "類別":
+      Restaurant.find()
+        .lean()
+        .sort({category: 'asc'}) //sort()排序，asc正序/desc反序
+        .then((restaurants) => res.render("index", { restaurants, sort }))
+        .catch(error => console.log(error))
+      break
+     case "地區":
+      Restaurant.find()
+        .lean()
+        .sort({location: 'asc'}) //sort()排序，asc正序/desc反序
+        .then((restaurants) => res.render("index", { restaurants, sort }))
+        .catch(error => console.log(error))
+      break 
+  }
+})
 
-// })
 
 
 module.exports = router
