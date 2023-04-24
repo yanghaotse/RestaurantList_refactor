4118e12a73ee746377a5ff2fb73d00dfed653db0
yang haotse <ha91252@gmail.com>
2023-04-24 10:54:43 +0800

    rename: routes/models -> routes/modules

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-const home = require('./models/home')
-const restaurants = require('./models/restaurants')
+const home = require('./modules/home')
+const restaurants = require('./modules/restaurants')
 
 router.use('/', home)
 
